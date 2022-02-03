52605e386254867e26879a119d3d696be6974afb
mona <csimonusa@outlook.com>
2022-02-03 18:21:59 -0500

    fixed type wasnt updating role

--- db/dbQueries.js
+++ db/dbQueries.js
@@ -37,7 +37,7 @@ class DB {
 
     updateEmployeeRole(employeeId, roleId) {
         return this.connection.promise().query(
-            'UPDATE employee SET manager_id = ? WHERE id = ?',
+            'UPDATE employee SET role_id = ? WHERE id = ?',
             [roleId, employeeId]
         )
     }
